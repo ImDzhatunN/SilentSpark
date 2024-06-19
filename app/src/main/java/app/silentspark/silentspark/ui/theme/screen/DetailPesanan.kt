3043285ebe303daf581866d7d7564a0742ddfff9
DzhatunNitha <nithaqhaini@gmail.com>
2024-06-19 10:19:47 +0700

    detail pesanan

---
+++ app/src/main/java/app/silentspark/silentspark/ui/theme/screen/DetailPesanan.kt
@@ -0,0 +1,12 @@
+package app.silentspark.silentspark.ui.theme.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailPesanan (){
+    Column {
+        Text(text = "INi detail pesanan")
+    }
+}
