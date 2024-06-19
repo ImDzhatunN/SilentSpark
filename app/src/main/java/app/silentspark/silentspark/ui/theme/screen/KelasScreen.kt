f7083e7345023a007d290d2acc3aa79e7afcd6d7
DzhatunNitha <nithaqhaini@gmail.com>
2024-06-20 00:23:17 +0700

    REVISI KELAS

--- app/src/main/java/app/silentspark/silentspark/ui/theme/screen/KelasScreen.kt
+++ app/src/main/java/app/silentspark/silentspark/ui/theme/screen/KelasScreen.kt
@@ -1,8 +1,8 @@
 package app.silentspark.silentspark.ui.theme.screen
 
-import app.silentspark.silentspark.R
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import app.silentspark.silentspark.R
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,8 +14,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.runtime.Composable
@@ -27,11 +27,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import app.silentspark.silentspark.dummy.DataDummy
 import app.silentspark.silentspark.dummy.DataKelas
 import app.silentspark.silentspark.model.Kelas
 import app.silentspark.silentspark.model.Pesanan
@@ -49,7 +49,7 @@ fun KelasScreen(
     listkelas: List<Kelas> = emptyList(),
     onBackClick: () -> Unit = {},
 ) {
-    var active by remember { mutableStateOf(false) }
+
 
     Scaffold(
         topBar = {
@@ -69,7 +69,7 @@ fun KelasScreen(
                         )
                         Spacer(modifier = modifier.weight(1f))
                         Text(
-                            text = "Kelas",
+                            text = "Daftar Pesanan",
                             style = TextStyle(
                                 fontSize = 16.sp,
                                 fontFamily = FontFamily(Font(R.font.poppins_semibold)),
@@ -98,6 +98,8 @@ fun KelasScreen(
                     }
 
                 }
+
+
             }
         }
     )
@@ -111,3 +113,5 @@ private fun KelasScreenPreview() {
         KelasScreen(listkelas = DataKelas.listKelas)
     }
 }
+
+
