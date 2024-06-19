9dcb14262579555ce36c7dc39f5d3a62e8a1d271
Aldykrisnaa <muhaldy2929@gmail.com>
2024-06-19 09:42:20 +0800

    Initial Commit

--- app/src/main/java/app/silentspark/silentspark/ui/theme/components/ItemPayment.kt
+++ app/src/main/java/app/silentspark/silentspark/ui/theme/components/ItemPayment.kt
@@ -16,12 +16,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import app.silentspark.silentspark.ui.theme.theme.Hijau
 import app.silentspark.silentspark.ui.theme.theme.poppinsFamily
 import app.silentspark.silentspark.ui.theme.theme.putih
 
@@ -33,7 +33,7 @@ fun ItemPayment(
         modifier = modifier
             .clip(RoundedCornerShape(16.dp))
             .fillMaxWidth()
-            .background(color = colorResource(id = R.color.green))
+            .background(color = Hijau)
     ) {
         Column(modifier.padding(start = 24.dp, top = 16.dp, bottom = 16.dp, end = 24.dp)) {
             Text(
