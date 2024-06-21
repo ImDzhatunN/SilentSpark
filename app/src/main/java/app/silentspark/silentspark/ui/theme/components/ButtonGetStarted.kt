b7824e229e0eb146ee293918aeee0d5083dc6d3c
Aajengsr <212110023@mhs.uqgresik.ac.id>
2024-06-21 15:52:08 +0700

    Button Get Started

--- app/src/main/java/app/silentspark/silentspark/ui/theme/components/ButtonGetStarted.kt
+++ app/src/main/java/app/silentspark/silentspark/ui/theme/components/ButtonGetStarted.kt
@@ -1,5 +1,6 @@
 package app.silentspark.silentspark.ui.theme.components
 
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
